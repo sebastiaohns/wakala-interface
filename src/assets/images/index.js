d8cd4a2d1a6a81b75bc148d4e3f6b9ddb03d20b3
Sebastião Henrique <sebastiaohns@gmail.com>
2021-10-26 23:07:56 -0300

    Adding empty home screen design

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,6 @@
-
-export const SLIDER_BUTTON = require("./slider-button.png")
-export const SLIDER_IMAGE_001 = require("./slider-001.png")
-export const SLIDER_IMAGE_002 = require("./slider-003.png")
-export const SLIDER_IMAGE_003 = require("./slider-002.png")
-export const SLIDER_IMAGE_004 = require("./slider-004.png")
+export const SLIDER_BUTTON = require("./slider-button.png");
+export const SLIDER_IMAGE_001 = require("./slider-001.png");
+export const SLIDER_IMAGE_002 = require("./slider-003.png");
+export const SLIDER_IMAGE_003 = require("./slider-002.png");
+export const SLIDER_IMAGE_004 = require("./slider-004.png");
+export const HOME_EMPTY = require("./home_empty.png");
