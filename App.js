6565394d0c9c0a71a78f410397a8d01a204cbfa0
Zeel <isamp100@gmail.com>
2021-11-11 22:51:39 +0530

    Fixed wrong paths and updated the Settings Screens (#188)
    * intial version of screen PinDoNotMathc
    
    * Added the Screen under Settings Pin Do Not Match
    
    * resolved errors and abided the requested changes
    
    * Added two screens under Help and Settings and updated the last Screen
    
    * Corrected errors from last push, adding Screens under Help and Settings
    
    * Added three screens: 2 Under Settings and 1 Under Help
    
    * Completely Revamped the PinDoNotMatch Code and added a screen under Settings
    
    * Added the Settings Screen / Main Panel of Settings
    
    * Resolved the fonts issue with SettingsScreen
    
    * Fixed an issue with wrong paths and resolved the mentioned comments
    
    * Created StyleSheets to group components and resolved the issues
    
    * Grouped the remaining components
    
    * Added remaining screens and fixed a few bugs
    
    * fixed a few bugs
    
    * Integrated the screens on Settings Page and fixed Help Screens
    
    * Delete yarn.lock
    
    Co-authored-by: Abijah Kajabika <34766858+AbijahKaj@users.noreply.github.com>

--- App.js
+++ App.js
@@ -26,8 +26,16 @@ import { COLORS, SIZES } from "./src/consts/theme";
 import VerifyNumber from "./src/screens/Auth/VerifyNumber";
 import ToC from "./src/screens/Auth/ToC";
 import AccountAddress from "./src/screens/Settings/AccountAddress";
-import PinDoNotMatch from "./src/screens/Settings/AccountAddress";
+import PinDoNotMatch from "./src/screens/Settings/PinDoNotMatch";
 import ContactSupportScreen from "./src/screens/Help/ContactSupportScreen";
+import PinSuccessScreen from "./src/screens/Settings/PinSuccessScreen";
+import RecoveryPhrase from "./src/screens/Settings/RecoveryPhrase";
+import SettingsScreen from "./src/screens/Settings/SettingsScreen";
+import HelpScreen from "./src/screens/Help/HelpScreen";
+import ResetAccountScreen from "./src/screens/Settings/ResetAccountScreen";
+import SelectCurrencyScreen from "./src/screens/Settings/SelectCurrencyScreen";
+import ContactScreen from "./src/screens/Help/ContactScreen";
+
 import SelectTopUpWithdraw from "./src/screens/AddDeposit/SelectTopUpWithdraw";
 import CashMpesa from "./src/screens/AddDeposit/CashMpesa";
 import AddFunds from "./src/screens/AddDeposit/AddFunds";
@@ -74,6 +82,37 @@ const Home = () => {
         component={HomeScreen}
         options={{ headerShown: false }}
       />
+      <Drawer.Screen
+        name="Support"
+        component={ContactSupportScreen}
+        // options={{ headerShown: false }}
+      />
+      <Drawer.Screen
+      name="PinSuccess"
+      component={PinSuccessScreen}
+      options={{ headerShown: false }}
+      />
+      <Drawer.Screen
+      name="Settings"
+      component={SettingsScreen}
+      options={{ headerShown: false }}
+      />
+      <Drawer.Screen
+      name="HelpScreen"
+      component={HelpScreen}
+      options={{ headerShown: false }}
+      />
+      <Drawer.Screen
+      name="Contact"
+      component={ContactScreen}
+      options={{ headerShown: false }}
+      />
+      <Drawer.Screen
+      name="SelectCurrency"
+      component={SelectCurrencyScreen}
+      options={{ headerShown: false }}
+      />
+
       <Drawer.Screen name="Account Address" component={AccountAddress} />
       <Drawer.Screen name="Pin" component={PinDoNotMatch} />
       <Drawer.Screen name="Support" component={ContactSupportScreen} />
@@ -111,6 +150,7 @@ const App = () => {
       <NavigationContainer>
         <RootStack.Navigator screenOptions={{ headerShown: false }}>
           <RootStack.Screen name="Landing" component={OnboardingScreen} />
+          
           <RootStack.Screen name="Signup">
             {(props) => <SignUpScreen {...props} onSignIn={handleSignIn} />}
           </RootStack.Screen>
@@ -119,6 +159,62 @@ const App = () => {
           <RootStack.Screen name="ToC">
             {(props) => <ToC {...props} onSignIn={handleSignIn} />}
           </RootStack.Screen>
+          <RootStack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+            name="Select Top up Withdraw"
+            component={SelectTopUpWithdraw}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+            name="Cash Mpesa"
+            component={CashMpesa}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+            name="Add Funds"
+            component={AddFunds}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+            name="RecoveryPhrase"
+            component={RecoveryPhrase}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+            name="PinDoNotMatch"
+            component={PinDoNotMatch}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+            name="Account Address"
+            component={AccountAddress}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+             name="ResetAccount"
+             component={ResetAccountScreen}
+             options={{ headerShown: false }}
+          />
+
+          <RootStack.Screen
+            name="Confirm Funds"
+            component={ConfirmFunds}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+            name="Send Mpesa"
+            component={SendMpesa}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+            name="Success"
+            component={Success}
+            options={{ headerShown: false }}
+          />
           <RootStack.Screen name="SetPIN" component={SetPIN} />
           <RootStack.Screen name="ConnectPhone" component={ConnectPhone} />
           <RootStack.Screen name="PhoneVerificationLoader" component={PhoneVerificationLoader} />
